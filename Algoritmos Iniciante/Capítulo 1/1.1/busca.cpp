149ceb2e7d8dc264bc8944d4156870a0e87ee3fa
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2015-05-18 17:19:35 -0300

    novos problemas

---
+++ Algoritmos Iniciante/Capítulo 1/1.1/busca.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	printf("%d\n", 4 * t);
+	return 0;
+}
+
+
+// s = 2*t
+// t = s / 2
+// 
+
+//s = 1/2*p
+// 2 * t = 1/ 2 *p
+// 4 * t = p
+
